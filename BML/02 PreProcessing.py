5b7f632c475262b347bac9f2ffe53562c272f84e
bunnerman <parnilrbalawade@gmail.com>
2026-08-13 10:10:50 +0530

    Update 02 PreProcessing.py

--- BML/02 PreProcessing.py
+++ BML/02 PreProcessing.py
@@ -2,19 +2,17 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
-# 1. Load data
-df = pd.read_csv("iris.csv")
+df = pd.read_csv("iris.csv", na_values=[''])
 
-# 2. Fill empty columns with their mathematical averages (means)
-num_vals = df.select_dtypes(include=["float64", "int64"]).columns
-df[num_vals] = df[num_vals].fillna(df[num_vals].mean())
+print(df.info())
+vals = df.select_dtypes(include=["float64", "int64"]).columns
+df[vals] = df[vals].fillna(df[vals].mean())
 
-# 3. Scale numerical length and width features to [0, 1] range
 scaler = MinMaxScaler()
-df[num_vals] = scaler.fit_transform(df[num_vals])
+df[vals] = scaler.fit_transform(df[vals])
 
-# 4. Encode categorical species labels into integers (0, 1, 2)
 encoder = LabelEncoder()
 df["Type"] = encoder.fit_transform(df["Type"])
 
 print(df)
+print(df.info())
