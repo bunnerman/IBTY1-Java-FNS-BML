a8957dddd54f99a7d5f6eceefc372ebf6ad34e2a
bunnerman <parnilrbalawade@gmail.com>
2026-09-03 00:30:48 +0530

    Update 09A Password Exception.java

--- Java/09A Password Exception.java
+++ Java/09A Password Exception.java
@@ -4,38 +4,31 @@ class Maine
 {
     public static void main(String[] args) 
     {
-        System.out.print("Enter password: ");
+        System.out.print("Enter pwd: ");
 		Scanner sc = new Scanner(System.in);
 		String st = sc.next();
 
 		CheckP obj = new CheckP();
-
-
-		try {
-			obj.checkPwd(st);
-		}
-		catch (AuthenException e) {
-			System.out.print("Exception Caught");
-		}
+		obj.checkPwd(st);
     }
 }
 
-class AuthenException extends Exception
+class pwdException extends RuntimeException
 {
-	public AuthenException() {
+	public pwdException() {
 		super();
 	}
-	public AuthenException(String msg) {
+	public pwdException(String msg) {
 		super(msg);
 	}
 }
 
 class CheckP {
-	public void checkPwd(String str) throws AuthenException {
+	public void checkPwd(String str) throws pwdException {
 		String pswd = "very_Secure_Password";
 		if (str.equals(pswd))
 			System.out.println("Correct Password");
 		else
-			throw new AuthenException("Auth Failure!");
+			throw new pwdException("Auth Failure!");
 	}
 }
