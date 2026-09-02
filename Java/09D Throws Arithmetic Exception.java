bd75ca63bb9ac6ab9ee0c07b625f15c07d91dd68
bunnerman <parnilrbalawade@gmail.com>
2026-09-03 00:40:08 +0530

    Create 09D Throws Arithmetic Exception.java

---
+++ Java/09D Throws Arithmetic Exception.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Maine
+{
+    public static void main(String[] args) 
+    {
+        System.out.print("Enter integer: ");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		checkA obj = new checkA();
+		
+		obj.checkAge(n);
+    }
+}
+
+class LowAgeException extends RuntimeException
+{
+	public LowAgeException() {
+		super();
+	}
+	public LowAgeException(String msg) {
+		super(msg);
+	}
+}
+
+class checkA {
+	public void checkAge(int n) {
+		if (n >= 18)
+			System.out.print("Adult");
+		else
+			throw new LowAgeException("Error: Minor");
+	}
+}
